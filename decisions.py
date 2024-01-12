41d6d0e0eb30a1384795ee38d69f7ab71f56d52c
NBTOONS45 <44732465+NBTOONS45@users.noreply.github.com>
2024-01-11 19:50:53 -0500

    Decisions example

---
+++ decisions.py
@@ -0,0 +1,13 @@
+import random
+number = random.randint(0,10)
+print(number)
+
+if number > 6:
+    print("big number")
+    print("big number")
+    print("big number")
+
+    print("number")
+
+    if number > 8:
+        print(" really big number")
